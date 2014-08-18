901e27facdf518e78291668cd12bae899332fcec
Alex Lemm <alex23lemm@gmail.com>
2014-08-19 00:28:36 +0200

    Add get_regional_data() function

--- utils.R
+++ utils.R
@@ -1,6 +1,7 @@
 library(jsonlite)
 library(dplyr)
 
+url <- config$country_url
 
 get_country_data <- function(url) {
   
@@ -14,15 +15,41 @@ get_country_data <- function(url) {
                                           function(e) e[["value"]][3] %>% as.numeric)
   
   processed_data <- raw_data %>% 
-    select(name, latitude, longitude) %>%
+    select(name, country, latitude, longitude) %>%
     mutate(
       latitude = as.numeric(latitude),
       longitude = as.numeric(longitude),
       people_of_concern,
       registered_syrian_refugees,
       persons_awaiting_registration
-    )
+    ) 
       
   return(processed_data)
   
+}
+
+
+get_regional_data <- function(url) {
+  
+  raw_data <- fromJSON(url)
+  
+  people_of_concern <- sapply(raw_data$population, 
+                              function(e) e[["value"]][1] %>% as.numeric)
+  registered_syrian_refugees <- sapply(raw_data$population, 
+                                       function(e) e[["value"]][2] %>% as.numeric)
+  persons_awaiting_registration <- sapply(raw_data$population,
+                                          function(e) e[["value"]][3] %>% as.numeric)
+  
+  processed_data <- raw_data %>% 
+    select(country, name, latitude, longitude) %>%
+    mutate(
+      latitude = as.numeric(latitude),
+      longitude = as.numeric(longitude),
+      people_of_concern,
+      registered_syrian_refugees,
+      persons_awaiting_registration
+    ) %>% 
+    filter(country != name)
+  
+  return(processed_data)
 }
