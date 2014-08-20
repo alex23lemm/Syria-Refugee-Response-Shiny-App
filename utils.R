9da74442437badc8548d46893cd3d7e607f6a18f
Alex Lemm <alex23lemm@gmail.com>
2014-08-20 21:35:50 +0200

    Delete deprecated function get_regional_data()

--- utils.R
+++ utils.R
@@ -43,27 +43,3 @@ get_UNHCR_population_data <- function(url, regions = FALSE) {
 }
 
 
-get_regional_data <- function(url) {
-  
-  raw_data <- fromJSON(url)
-  
-  people_of_concern <- sapply(raw_data$population, 
-                              function(e) e[["value"]][1] %>% as.numeric)
-  registered_syrian_refugees <- sapply(raw_data$population, 
-                                       function(e) e[["value"]][2] %>% as.numeric)
-  persons_awaiting_registration <- sapply(raw_data$population,
-                                          function(e) e[["value"]][3] %>% as.numeric)
-  
-  processed_data <- raw_data %>% 
-    select(country, name, latitude, longitude) %>%
-    mutate(
-      latitude = as.numeric(latitude),
-      longitude = as.numeric(longitude),
-      people_of_concern,
-      registered_syrian_refugees,
-      persons_awaiting_registration
-    ) %>% 
-    filter(country != name)
-  
-  return(processed_data)
-}
