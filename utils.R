76066ef04b77e7da3b1803d145a497bd4cd4bb8d
Alex Lemm <alex23lemm@gmail.com>
2014-08-20 23:47:56 +0200

    Add new functions: create_pyramid_plot(), tidy_demographic_data()

--- utils.R
+++ utils.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 library(dplyr)
+library(ggplot2)
 
 
 get_UNHCR_population_data <- function(url, regions = FALSE) {
@@ -38,8 +39,47 @@ get_UNHCR_population_data <- function(url, regions = FALSE) {
   if(regions)
     processed_data <- filter(processed_data, country != name)
      
-  return(processed_data)
+  return(processed_data) 
+}
+
+
+
+tidy_demographic_data <- function(demographic_data) {
   
+  demographic_data <- demographic_data %>% 
+    select(name, ends_with('M'), ends_with('F')) %>%
+    gather(age, numb_of_persons, -name) %>% 
+    separate(age, into = c('age', 'gender'), sep = -2) %>%
+    group_by(name) %>%
+    mutate(
+      percent = round(numb_of_persons / sum(numb_of_persons) * 100, digits = 1)
+    ) %>% 
+    ungroup %>%
+    mutate(
+      age = factor(age, levels = c('04', '511', '1217', '1859', '60'),
+                   labels = c('0-4', '5-11', '12-17', '18-59', '60+'))
+    )
+  
+  return(demographic_data)
+}
+
+
+create_pyramid_plot <- function(demographic_data, country_name) {
+  
+  demographic_data <- demographic_data %>% filter(name == country_name)
+  
+  g <- ggplot(demographic_data, aes(age, percent, fill = gender)) +
+    geom_bar(data = filter(demographic_data, gender == 'M'), stat = 'identity') +
+    geom_bar(aes(y = percent * -1), filter(demographic_data, gender == 'F'), 
+             stat = 'identity') +
+    geom_text(aes(label = percent), filter(demographic_data, gender == 'M')) +
+    geom_text(aes(label = percent, y = percent * -1), filter(demographic_data, 
+                                                             gender == 'F')) +
+    coord_flip() +
+    theme_bw() +
+    scale_fill_brewer(palette = 'Set1')
+  
+  return(g)
 }
 
 
