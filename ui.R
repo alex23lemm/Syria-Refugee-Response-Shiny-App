d61e7c046250b2509f8b808420e57d6facbf43ad
Alex Lemm <alex23lemm@gmail.com>
2014-08-22 00:18:05 +0200

    Add navbarPage and tabPanels

--- ui.R
+++ ui.R
@@ -1,16 +1,35 @@
 library(shiny)
 library(leaflet)
 
-shinyUI(fluidPage(
+shinyUI(navbarPage('Syria Regional Refugee Response',
+                               
+  tabPanel('Regional Overview',
+           
+    includeCSS('style.css'), 
+           
+    leafletMap(
+      "map", "100%", 400,
+      initialTileLayer = "http://a.tiles.mapbox.com/v3/unhcr.map-8bkai3wa/{z}/{x}/{y}.png",    
+      initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
+      options=list(
+        center = c(34.398, 40.649),
+        zoom = 5
+      )
+     ),
     
-  leafletMap(
-    "map", "100%", 400,
-    #initialTileLayer ="http://a.tiles.mapbox.com/v3/mapbox.control-room/{z}/{x}/{y}.png",
-    initialTileLayer = "http://a.tiles.mapbox.com/v3/unhcr.map-8bkai3wa/{z}/{x}/{y}.png",    
-    initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
-    options=list(
-      center = c(34.398, 40.649),
-      zoom = 5
-    ))
+     fluidRow(
+       column(8, offset = 3,
+         h2('Syrian Refugees in the Middle East')
+       )
+     )
+
+    
+  ),
+  
+  tabPanel('About'
+    
+  )
+    
+
   
   ))
