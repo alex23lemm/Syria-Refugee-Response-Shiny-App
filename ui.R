10491983c941465959821a642a40803ba485c97e
Alex Lemm <alex23lemm@gmail.com>
2014-08-23 23:30:44 +0200

    Add action button which allows to download new data from the UN server

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ shinyUI(navbarPage('Syria Regional Refugee Response',
     includeCSS('style.css'), 
            
     leafletMap(
-      "map", "100%", 400,
+      "map", width="100%", height="400px",
       initialTileLayer = "http://a.tiles.mapbox.com/v3/unhcr.map-8bkai3wa/{z}/{x}/{y}.png",    
       initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
       options=list(
@@ -19,9 +19,25 @@ shinyUI(navbarPage('Syria Regional Refugee Response',
     
      fluidRow(
        column(8, offset = 3,
-         h2('Syrian Refugees in the Middle East')
+         h2('Syrian Refugees in the Middle East'),
+         p(tags$i(class = 'icon-time'), 'Data last updated on:', textOutput('date')),
+         actionButton('downloadButton', 'Refresh data')
+         
        )
-     )
+     ),
+    
+    fluidRow(
+      column(4,
+        selectInput('country_name', label = 'test', choices = list('Turkey', 'Lebanon',
+                                                                   'Iraq', 'Jordan', 'Egypt'), 
+                    selected = 'Turkey')     
+             
+      ),
+      column(8,
+        plotOutput('pyramid_plot', width='70%', height='280px')
+      )
+    )
+    
 
     
   ),
