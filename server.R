f53099c7e363121f3f4f992a7cf6847f1ba3d2cd
Alex Lemm <alex23lemm@gmail.com>
2014-08-23 23:30:00 +0200

    Add makeReactiveBinding calls so that initial user session can show dumped data at first

--- server.R
+++ server.R
@@ -1,10 +1,45 @@
-library(shiny)
 
+# Load libraries, source files and config files --------------------------------
+
+library(shiny)
+library(yaml)
+
+source('utils.R')
+source('downloaded_data.R')
+config <- yaml.load_file('config.yml')
 
 
 shinyServer(function(input, output, session) {
   
+  source('downloaded_data.R', local = TRUE)
+  
+  makeReactiveBinding('unhcr_data')
+  makeReactiveBinding('demographic_data')
+  makeReactiveBinding('date_downloaded')
+  
+  source('downloaded_data.R')
+  
+  observe({
+    if(input$downloadButton == 0)
+      return()
+    
+    unhcr_data <<- get_UNHCR_population_data(config$country_url)
+    demographic_data <<- tidy_demographic_data(unhcr_data)
+    date_downloaded <<- date()
+  })
+  
+  
+  output$date <- renderText({
+    
+    date_downloaded
+  })
+  
+  
+  output$pyramid_plot <- renderPlot({
+    create_pyramid_plot(demographic_data, input$country_name)
+  })
+  
   map <- createLeafletMap(session, 'map')
   
-  
+
 })
