87aab9851a4c82345f0b4386150782e1aaa12f14
Alex Lemm <alex23lemm@gmail.com>
2014-08-24 02:25:40 +0200

    Add new textOutput as heading for the pyramid plot

--- server.R
+++ server.R
@@ -13,15 +13,19 @@ shinyServer(function(input, output, session) {
   
   source('downloaded_data.R', local = TRUE)
   
+  # Create new reactive variable
+  selectedCountry <- 'Turkey'
+  makeReactiveBinding('selectedCountry')
   
-  #Turn sourced variables into reactive variables
+  
+  # Turn sourced variables into reactive variables
   makeReactiveBinding('unhcr_data')
   makeReactiveBinding('demographic_data')
   makeReactiveBinding('date_downloaded')
   
-  
   # In every session the dumped data is shown intitally. When the user clicks 
-  # on the action button new data will be downloaded from the UN servers
+  # on the action button new data will be downloaded from the UN servers via 
+  # the funciton in utils.R
   observe({
     if(input$downloadButton == 0)
       return()
@@ -33,13 +37,17 @@ shinyServer(function(input, output, session) {
   
   
   output$date <- renderText({
-    
     date_downloaded
+  })
+  
+  output$pyramidPlotLabel <- renderText({
+    paste('Demographic refugee breakdown for', selectedCountry)
+    
   })
   
   
   output$pyramid_plot <- renderPlot({
-    create_pyramid_plot(demographic_data, input$country_name)
+    create_pyramid_plot(demographic_data, selectedCountry)
   })
   
   
@@ -70,6 +78,7 @@ shinyServer(function(input, output, session) {
     
     isolate({
       country <- filter(unhcr_data, name == event$id)
+      selectedCountry <<- country$name
       content <- as.character(tagList(
         tags$strong(country$name)
         ))
